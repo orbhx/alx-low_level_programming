98aa0d29a3050a52e2a972528041009711127e36
orbhx <madutaiwo@gmail.com>
2022-06-02 06:39:41 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * The function will output a string
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *a = "\"Programming is like building a multilingual puzzle";
+
+puts(a);
+
+return (0);
+
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+
+/**
+ * main - Entry point
+ *
+ * The function will output the type size of in 32bits environment
+ * or the type size of in 64bits environment
+ * compile it in 64 bits with no warning
+ * compile it in 32 bits with waring
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+printf("Size of a float: %lu byte(s)\n", sizeof(float));
+
+return (0);
+
+}
