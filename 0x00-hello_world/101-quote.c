ca1ff8e9cf4a37917519e880f12057d974945320
orbhx <madutaiwo@gmail.com>
2022-06-02 07:07:15 -0700

    0x00-hello_world

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * print a string
+ *
+ * Return: 1 on success.
+ */
+
+int main(void)
+{
+	char *str;
+	int i;
+
+	str = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	for (i = 0; i < 58; i++)
+	{
+		putchar(str[i]);
+	}
+	putchar('\n');
+	return (1);
+}
