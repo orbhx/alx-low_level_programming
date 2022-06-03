e0fe777ca46bc51d493a6b4006911ebdc1d9ffba
orbhx <madutaiwo@gmail.com>
2022-06-03 08:09:19 -0700

    0x01-variables_if_else_while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
 /**
  *
- * main - Entry point
+ * main - main point
  *
  * Return: Always 0 (Success)
  *
@@ -12,10 +9,11 @@
 
 int main(void)
 {
-	int i;
+	int num;
 
-	for (i = 0; i < 10; i++)
-		printf("%d", i);
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+
 	printf("\n");
 
 	return (0);
