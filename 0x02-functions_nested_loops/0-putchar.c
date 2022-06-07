ac52358026c3829ee877c0090f186d6c6c170722
orbhx <madutaiwo@gmail.com>
2022-06-07 07:32:05 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar.h"
+
+/**
+ * main - print the string "_putchar"
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	char *str;
+	int i;
+
+	str = "_putchar";
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
